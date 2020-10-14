1c5ef7245435dd71d1a61ab34d69aa053b2eeb99
rythmE <poiuwertion@gmail.com>
2020-10-14 16:01:20 +0800

    Updated requirements

--- setup.py
+++ setup.py
@@ -84,7 +84,11 @@ setup(
     packages=[
         'xblockutils',
     ],
-    install_requires=load_requirements('requirements/base.in'),
+    install_requires=[
+        'XBlock==1.2.9',
+        'Mako',
+        'futures==3.2.0',
+    ],
     package_data=package_data("xblockutils", ["public", "templates", "templatetags"]),
     url='https://github.com/edx/xblock-utils',
 )
